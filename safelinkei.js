a3ca2d57969a0d6475594cb355e16501c79d7706
Hayatul1 <makky300687@gmail.com>
2023-10-28 23:52:20 +0700

    Add files via upload

--- safelinkei.js
+++ safelinkei.js
@@ -18,16 +18,12 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
-var xxx=null;
-if(x=="1"){xxx="post1.html"} 
-if(x=="2"){xxx="post2.html"} 
-if(x=="3"){xxx="post3.html"} 
-if(x=="4"){xxx="post4.html"}a_to_ve[i].href="https://safelinkei.blogspot.com/2023/10/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://safelinkei.blogspot.com/p/link-generator.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
 {a_to_vk.innerHTML+=a_to_va;}}
 function a_to_fa()
 {var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
+protected_links = "YourDomain.com,AnotherDomain.com,youtube.com,facebook.com,google.com";
+auto_safelink();
